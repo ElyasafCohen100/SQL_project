c5633f9876a769b42fc52eec1e40e7d744f8720d
Tsuriya1 <Tsuriya60@gmail.com>
2024-05-22 02:05:27 +0300

    added the dropTable, located our tables created in PLSQL :)

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # SQL_project
-bla bla
---
+++ dropTables.sql
@@ -0,0 +1,11 @@
+DROP TABLE Loans
+
+DROP TABLE LoanDepartment
+
+DROP TABLE clients
+
+DROP TABLE workers
+
+DROP TABLE LoanRequests
+
+DROP TABLE ClosedLoans
